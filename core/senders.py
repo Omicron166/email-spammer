1d453eae5b347bbc9839c793ab7503d040925f98
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-11-17 21:14:14 +0100

    implement the new template engine

--- core/senders.py
+++ core/senders.py
@@ -5,11 +5,12 @@ from core.connection import SMTPConnection
 from core.templates import Template, TemplateEngine
 
 class Sender(Thread):
-    def __init__(self, server: SMTPConnection, timeout: int = 1) -> None:
+    def __init__(self, server: SMTPConnection, options: dict, timeout: int = 1) -> None:
         super().__init__()
         self.server = server
         self.timeout = timeout
         self.generator = TemplateEngine()
+        self.options = options
 
     def set_template(self, template: Template) -> None:
         self.generator.set_template(template)
@@ -23,8 +24,8 @@ class Sender(Thread):
         super().start()
 
 class Spammer(Sender):
-    def __init__(self, server: SMTPConnection, victims: list, timeout: int = 1) -> None:
-        super().__init__(server, timeout)
+    def __init__(self, server: SMTPConnection, victims: list[str], options: dict, timeout: int = 1) -> None:
+        super().__init__(server, options, timeout)
         self.victims = victims
         self.start()
     
@@ -34,7 +35,9 @@ class Spammer(Sender):
                 victim = self.victims.pop()
             except:
                 break
-            email = self.generator.gen_email(victim)
+            self.options['victim'] = victim.split('@')[0]
+            self.options['email'] = victim
+            email = self.generator.gen_email(self.options)
             self.server.sendmail(victim, email)
             sleep(self.timeout)
     
