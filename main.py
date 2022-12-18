e52d779932568f7c6063f5eb39b1e3c425190441
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-12-18 13:59:23 +0100

    add the first commands

--- main.py
+++ main.py
@@ -9,4 +9,19 @@ while True:
     cmd = input('email-spammer> ')
     args = cmd.split()
     command = args.pop(0)
+
     if command == '': continue
+
+    if command == 'template':
+
+        if args[0] == 'list':
+            # Print a list of the available templates
+            ## Dev note: temporal code, must be replaced with a list of just the template names
+            print(storage.templates)
+
+        elif args[0] == 'show':
+            if not storage.templates.get(args[1]):
+                print(f'[!] Template {args[1]} not found')
+                continue
+            print(storage.templates[args[1]])
+
